b554d8c98ae1ea5a6d00aa2809f16f66d751c008
yobukodori <32874862+yobukodori@users.noreply.github.com>
2020-09-24 17:50:01 +0900

    Add files via upload

---
+++ sample/encoding-shift_js.js
@@ -0,0 +1,4 @@
+(function(){
+	"use strict" ;
+	alert("The encoding of this file is shift_jis\nWith Japanese characters:このファイルのエンコーディングはshift_jisです");
+})();
---
+++ sample/hello.js
@@ -0,0 +1,4 @@
+(function(){
+	"use strict";
+	alert("hello from yobukodori.github.io");
+})();
